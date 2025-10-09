e27edca53e3e78eab940185acc6fef0420d0b65c
adsmdf <adamsolimani2014@gmail.com>
2025-10-09 17:19:15 -0500

    Killing tmux from the first .sh

--- useful_shell_scripts/run_prepare_container.sh
+++ useful_shell_scripts/run_prepare_container.sh
@@ -28,6 +28,7 @@ tmux send-keys -t $SESSION:0.0 "echo 'Copying CSV File...'" C-m
 tmux send-keys -t $SESSION:0.0 "cp -r data/ install/precision_delivery/share/precision_delivery/" C-m
 
 tmux attach -t $SESSION
+tmux kill-session -t $SESSION 2>/dev/null
 
 # --- Run multisines script automatically ---
 if [ -x "./run_multisines.sh" ]; then
