3233ddee233e6e0233159438cac4e3582121b58e
adsmdf <adamsolimani2014@gmail.com>
2025-10-13 10:31:34 -0500

    Fixed C-m

--- useful_shell_scripts/run_multisines.sh
+++ useful_shell_scripts/run_multisines.sh
@@ -56,7 +56,7 @@ tmux send-keys -t $SESSION:0.1 "ros2 run precision_delivery signal_sub.py" C-m
 # rosbag (bottom left)
 tmux send-keys -t $SESSION:0.2 "echo 'Recording /mavros/rc/in...'" C-m
 tmux send-keys -t $SESSION:0.2 "cd data/Output_Data/" C-m
-tmux send-keys -t $SESSION:0.2 "sudo rm -rf ros2bag"
+tmux send-keys -t $SESSION:0.2 "sudo rm -rf ros2bag" C-m
 tmux send-keys -t $SESSION:0.2 "ros2 bag record -o ros2bag /mavros/rc/in" C-m
 
 # signal_pub (bottom right)
